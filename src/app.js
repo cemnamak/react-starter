4933304d40ba41904b57b36425f32faa0251f29e
cem <r_vassili@hotmail.com>
2021-08-18 22:53:30 +0300

    react-starter

---
+++ src/app.js
@@ -0,0 +1,5 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './styles/style.scss';
+
+ReactDOM.render(<h1>react-starter</h1>,document.getElementById("root"));
---
+++ webpack.config.js
@@ -0,0 +1,27 @@
+const path = require('path');
+
+module.exports = {
+  entry: './src/app.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js',
+  },
+  devServer: {
+    contentBase: path.join(__dirname, 'dist'),
+    port: 8080,
+  },
+  mode:'development',
+  module:{
+    rules:[
+      {
+        test: /\.js$/,
+        loader:'babel-loader',
+        exclude:/node_modules/
+      },
+      {
+        test: /\.scss$/,
+        use:['style-loader','css-loader','sass-loader']
+      }
+  ]
+  }
+};
